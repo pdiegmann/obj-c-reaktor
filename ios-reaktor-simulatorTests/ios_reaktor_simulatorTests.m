11e7d7c41ba9161a327c087e451f31f82147ba19
Phil Diegmann <pdiegmann@gmail.com>
2012-11-15 23:06:55 +0100

    Fixed some comparability-issues between the iOS-Device- and iOS-Simulator-Versions

---
+++ ios-reaktor-simulatorTests/ios_reaktor_simulatorTests.m
@@ -0,0 +1,32 @@
+//
+//  ios_reaktor_simulatorTests.m
+//  ios-reaktor-simulatorTests
+//
+//  Created by Phil Diegmann on 15.11.12.
+//  Copyright (c) 2012 Phil Diegmann. All rights reserved.
+//
+
+#import "ios_reaktor_simulatorTests.h"
+
+@implementation ios_reaktor_simulatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ios-reaktor-simulatorTests");
+}
+
+@end
